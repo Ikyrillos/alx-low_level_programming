909937707cf2a7f321a0cf198811582745563fd0
Ikyrillos <kyrillosmaherfekri@gmail.com>
2022-10-18 17:41:25 -0700

    new

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints _putchar as a message.
+ *
+ * Return: 0 nno errors
+ */
+int main(void)
+{
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count, length;
+
+	length = sizeof(str) / sizeof(int);
+	for (count = 0; count < length; count++)
+	{
+		putchar(str[count]);
+	}
+	putchar('\n');
+	return (0);
+}
