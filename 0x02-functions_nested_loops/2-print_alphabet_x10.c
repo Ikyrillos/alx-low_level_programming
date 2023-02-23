d668d938b37b9936c228de73e83a71601291947d
Ikyrillos <kyrillosmaherfekri@gmail.com>
2023-02-22 18:03:23 -0800

    new

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,13 +7,13 @@
  *     */
 void print_alphabet_x10(void)
 {
-	int i;
-       for (i = 0; i < 10; i++)
-       { 
-	for (i = 97; i <= 122; i++)
+	int count = 0;
+	char letter;
+
+	while (count++ <= 9)
 	{
-		_putchar(i);
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
 	}
-	_putchar('\n');
-       }
 }
